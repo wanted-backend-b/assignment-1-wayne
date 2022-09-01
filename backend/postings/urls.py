dcafd2accf72cb809636dfa65feef082374d89a3
HyeonWooJo <65996045+HyeonWooJo@users.noreply.github.com>
2022-09-01 20:10:19 +0900

    Revert "Feat: 공지사항 API 구현"

--- backend/postings/urls.py
+++ backend/postings/urls.py
@@ -1,17 +1,10 @@
 from django.urls import path
 
-from postings.views import NoticeView
+from postings.views import FreeBoardListView, FreeBoardDetailView
 
 urlpatterns = [
-<<<<<<< HEAD
     path("freeboards", FreeBoardListView.as_view()),
     path("freeboards/detail", FreeBoardDetailView.as_view()),
     path("freeboards/detail/<int:posting_id>", FreeBoardDetailView.as_view()),
     
-=======
-    path("notices", FreeBoardListView.as_view()),
-    path("notices/detail", FreeBoardDetailView.as_view()),
-    path("notices/detail/<int:posting_id>", FreeBoardDetailView.as_view()),
-
->>>>>>> bf20324 (Feat: 공지사항 API 구현)
 ]
