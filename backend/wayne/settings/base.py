f542df897d6053c7928c99e297434e5ab199aeb7
gkgkgkgkdlel <gkgkgkgkdlel@naver.com>
2022-09-01 23:29:06 +0900

    Feat: 나이대별 게시판 이용 통계 api 구현

--- backend/wayne/settings/base.py
+++ backend/wayne/settings/base.py
@@ -80,24 +80,13 @@ WSGI_APPLICATION = "wayne.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "wanted_posting",
-        "USER": "root",
-        "PASSWORD": "0000",
-        "HOST": "localhost",
-        "PORT": "3306",
-
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
