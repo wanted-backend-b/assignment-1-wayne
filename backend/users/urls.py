be3843d9ef35bb67110323eda538fd94eb055e42
lution88 <lution88@gmail.com>
2022-09-02 11:27:33 +0900

    setup: user API 기본 세팅

---
+++ backend/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('signup/', views.SignUpView.as_view()),
+    path('login/', views.LogInView.as_view()),
+    path('withdrawal/', views.WithdrawalView.as_view()),
+]
--- backend/wayne/urls.py
+++ backend/wayne/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 urlpatterns = [
     path("postings", include("postings.urls")),
     path("statistics/", include('statistics_aggregation.urls')),
+    path("users/", include("users.urls")),
 ]
