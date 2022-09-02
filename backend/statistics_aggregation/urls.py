8d22e79c33a9561f1c0ee37ea81b1898d667857e
HyeonWooJo <65996045+HyeonWooJo@users.noreply.github.com>
2022-09-02 09:25:55 +0900

    Revert "Feat: 자유게시판 API 구현"

--- backend/statistics_aggregation/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import FreeGenderView, NoticeGenderView, OperateGenderView
-
-urlpatterns = [
-    path('gender/operate/', OperateGenderView.as_view()),
-    path('gender/notice/', NoticeGenderView.as_view()),
-    path('gender/free/', FreeGenderView.as_view()),
-
-]
