facc498458491b65141e2925eb6d47137811910f
HyeonWooJo <HyeonWooJo@github.com>
2022-09-03 16:33:01 +0900

    Feat: 자유 게시판 댓글, 조회수 API unit test 구현

--- backend/postings/tests.py
+++ backend/postings/tests.py
@@ -139,7 +139,7 @@ class FreeBoardDetailView(TestCase):
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json(), {"message" : "SUCCESS"})
 
-    # # 자유게시판 상세 삭제 API unit test
+    # 자유게시판 상세 삭제 API unit test
     def test_success_detail_view_delete(self):
         client   = Client()
         header   = {"HTTP_Authorization" : self.token}
@@ -152,3 +152,96 @@ class FreeBoardDetailView(TestCase):
 
         self.assertEqual(response.status_code, 204)
         self.assertEqual(response.json(), {"message" : "DATA_DELETED"})
+
+
+# 자유게시판 댓글 API unit test
+class FreeBoardCommentView(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id              = 1,
+            email           = 'aasdfa@naver.com',
+            psword          = '123AAV@@#',
+            name            = '홍길동',
+            level           = 1,
+            gender          = '남',
+            age             = 23,
+            phone_number    = '010-2222-3333',
+        )
+
+        FreeBoardPosting.objects.create(
+            id      = 1,
+            title   = 'Hi',
+            context = 'Hi',
+            user    = User.objects.get(id=1)
+        )
+
+        self.token = jwt.encode({'user_id':User.objects.get(id=1).id}, os.environ.get("SECRET"), 
+                        os.environ.get("ALGORITHM"))
+
+    def tearDown(self):
+        User.objects.all().delete()
+        FreeBoardPosting.objects.all().delete()
+        FreeComment.objects.all().delete()
+        FreeView.objects.all().delete()
+
+    # 자유게시판 댓글 포스팅 API unit test
+    def test_success_comment_view_post(self):
+        client   = Client()
+        header   = {"HTTP_Authorization" : self.token}
+        body     = {
+            "comment"    : "Hello",
+            "posting_id" : 1
+        }
+
+        response = client.post("/postings/freeboards/comment", **header, 
+                    content_type='application/json',
+                    data=json.dumps(body))
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {"message" : "SUCCESS"})
+
+
+# 자유게시판 조회 API unit test
+class FreeBoardView(TestCase):
+    def setUp(self):
+        User.objects.create(
+            id              = 1,
+            email           = 'aasdfa@naver.com',
+            psword          = '123AAV@@#',
+            name            = '홍길동',
+            level           = 1,
+            gender          = '남',
+            age             = 23,
+            phone_number    = '010-2222-3333',
+        )
+
+        FreeBoardPosting.objects.create(
+            id      = 1,
+            title   = 'Hi',
+            context = 'Hi',
+            user    = User.objects.get(id=1)
+        )
+
+        self.token = jwt.encode({'user_id':User.objects.get(id=1).id}, os.environ.get("SECRET"), 
+                        os.environ.get("ALGORITHM"))
+
+    def tearDown(self):
+        User.objects.all().delete()
+        FreeBoardPosting.objects.all().delete()
+        FreeComment.objects.all().delete()
+        FreeView.objects.all().delete()
+
+    # 자유게시판 댓글 포스팅 API unit test
+    def test_success_view_post(self):
+        client   = Client()
+        header   = {"HTTP_Authorization" : self.token}
+        body     = {
+            "posting_id" : 1
+        }
+
+        response = client.post("/postings/freeboards/view", **header, 
+                    content_type='application/json',
+                    data=json.dumps(body))
+
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.json(), {'message' : 'SUCCESS', 'count' : 1})
