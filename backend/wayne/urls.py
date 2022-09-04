e695c220ec4cbed898fbd35716b5261e6a580436
kdh92417 <kdh92417@gmail.com>
2022-09-04 16:45:45 +0900

    :hammer: Fix: 슬래시 추가
    루트 경로에 있는 path에 슬래시(/) 추가

--- backend/wayne/urls.py
+++ backend/wayne/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 urlpatterns = [
-    path("postings", include("postings.urls")),
-    path("statistics", include("statistics_aggregation.urls")),
-    path("users", include("users.urls")),
+    path("postings/", include("postings.urls")),
+    path("statistics/", include("statistics_aggregation.urls")),
+    path("users/", include("users.urls")),
 ]
