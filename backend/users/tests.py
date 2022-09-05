3b298d3b933088a9ec54f1ac1ca49d17148c15cc
lution88 <lution88@gmail.com>
2022-09-05 10:18:18 +0900

    feat: TestSignUpView 기본셋업1

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+import json
+import jwt
+import os
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from postings.models import FreeBoardPosting, FreeComment, FreeView
+from .models import User
+
+
+class TestSignUpView(TestCase):
+    def setUp(self):
+        self.client = Client()
